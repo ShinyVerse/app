b64dbe8b88597612340556aa59e675c21d9f42a0
Laura Jackson <playeronereadystart@gmail.com>
2020-06-25 13:32:41 +0100

    Updates

---
+++ precache-manifest.3659aa5a99d4def2824b03cd7aab4d73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb46021d15016e2d041511fc2a0d5902",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "2575779dafa36c3ab971",
+    "url": "/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/app/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2575779dafa36c3ab971",
+    "url": "/app/static/js/main.b96f8af4.chunk.js"
+  },
+  {
+    "revision": "aca9cec55175d18095b8",
+    "url": "/app/static/js/runtime-main.d0c462e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
